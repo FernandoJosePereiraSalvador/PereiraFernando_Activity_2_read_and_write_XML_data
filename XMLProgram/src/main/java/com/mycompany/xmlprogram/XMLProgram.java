10a86eaadc5736cadc16feec64bd5e680d679c40
FernandoPereiraSalvador <fernandoplanet9000@gmail.com>
2023-09-28 22:07:17 +0200

    Correccion
    Se ha corregido la estructura de carpetas
    
    Se ha completado el código, se ha documentado y se ha añadido un control de errores

--- XMLProgram/src/main/java/com/mycompany/xmlprogram/XMLProgram.java
+++ XMLProgram/src/main/java/com/mycompany/xmlprogram/XMLProgram.java
@@ -2,19 +2,16 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
  */
-
-package com.mycompany.documentacion;
+package com.mycompany.xmlprogram;
 
 /**
  *
  * @author Fernando
  */
-
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
@@ -29,22 +26,17 @@ import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
-
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 /**
- *
- * @author samuel
- */
-/**
  * Clase que representa operaciones con archivos XML.
  */
 class fileXML {
-    
+
     /**
      * Abre un archivo XML y devuelve su contenido como un objeto Document.
-     * 
+     *
      * @param name El nombre del archivo XML que se va a abrir.
      * @return Un objeto Document que representa el contenido del archivo.
      * @throws IOException Si ocurre un error al abrir el archivo.
@@ -53,7 +45,7 @@ class fileXML {
      * @throws FileNotFoundException Si el archivo XML especificado no se encuentra.
      */
     public Document OpenXML(String name) throws IOException, SAXException, ParserConfigurationException, FileNotFoundException {
-        
+
         // Crea una instancia del analizador de documentos XML.
         DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
         DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
@@ -71,7 +63,7 @@ class fileXML {
 
     /**
      * Imprime los módulos contenidos en el elemento raíz del documento XML.
-     * 
+     *
      * @param root El elemento raíz del documento XML.
      */
     public void printModules(Element root) {
@@ -83,12 +75,13 @@ class fileXML {
             System.out.println("Nombre: " + e.getElementsByTagName("name").item(0).getFirstChild().getNodeValue());
             System.out.println("Horas: " + e.getElementsByTagName("hours").item(0).getFirstChild().getNodeValue());
             System.out.println("Tiempo: " + e.getElementsByTagName("time").item(0).getFirstChild().getNodeValue());
+            System.out.println("Student: " + e.getElementsByTagName("student").item(0).getFirstChild().getNodeValue());
         }
     }
 
     /**
      * Escribe un nuevo módulo en un archivo XML.
-     * 
+     *
      * @param file El archivo XML en el que se va a escribir el módulo.
      * @throws SAXException Si ocurre un error de análisis SAX.
      * @throws ParserConfigurationException Si ocurre un error de configuración del analizador XML.
@@ -105,7 +98,7 @@ class fileXML {
         // Crear un nuevo documento XML.
         Document doc = dBuilder.newDocument();
         try {
-            // Crear elementos.
+            // Crear elementos
             Element rootElement = doc.createElement("course");
             doc.appendChild(rootElement);
 
@@ -122,26 +115,27 @@ class fileXML {
         StreamResult result = new StreamResult(new FileOutputStream(file));
         trans.transform(source, result);
     }
-    
+
     /**
      * Crea un nuevo modulo
-     * 
+     *
      * @param doc El documento XML
      * @return Un nuevo modulo
      */
-    public static Element crearModulo(Document doc){
-        
+    public static Element crearModulo(Document doc) {
+
+        // Crea nuevos elementos
         Element module = doc.createElement("module");
         Element name = doc.createElement("name");
         Element hours = doc.createElement("hours");
         Element currentTime = doc.createElement("time");
         Element student = doc.createElement("student");
-            
+
         // Configurar valores para el nuevo módulo.
         name.appendChild(doc.createTextNode("Acceso a datos"));
         hours.appendChild(doc.createTextNode(Integer.toString(8)));
         student.appendChild(doc.createTextNode("Fernando"));
-        
+
         LocalDateTime now = LocalDateTime.now();
         currentTime.appendChild(doc.createTextNode(now.toString()));
 
@@ -150,62 +144,111 @@ class fileXML {
         module.appendChild(hours);
         module.appendChild(currentTime);
         module.appendChild(student);
-        
+
         return module;
     }
-    
-    public void findModule(String moduleName, Document doc) {
-        NodeList modules = doc.getElementsByTagName("module");
-        for (int i = 0; i < modules.getLength(); i++) {
-        Element module = (Element) modules.item(i);
-        String name = module.getElementsByTagName("name").item(0).getTextContent();
 
-        if (name.equalsIgnoreCase(moduleName)) {
-                // Se encontró el módulo, muestra su información.
-            System.out.println("Nombre: " + name);
-            System.out.println("Horas: " + module.getElementsByTagName("hours").item(0).getTextContent());
-            System.out.println("Tiempo: " + module.getElementsByTagName("time").item(0).getTextContent());
-            System.out.println("Estudiante: " + module.getElementsByTagName("student").item(0).getTextContent());
-            return; // Termina la búsqueda.
+    /**
+     *
+     * Busca un módulo por su nombre en un documento XML y muestra su información si se encuentra.
+     *
+     * @param moduleName El nombre del módulo que se va a buscar.
+     * @param doc El objeto Document que representa el contenido del archivo XML.
+     */
+    public void findModule(String moduleName, Document doc) {
+
+        // Variable para saber si lo hemos encontrado
+        boolean encontrado = false;
+        // Obtiene una lista de todos los elementos "module"
+        NodeList modules = doc.getElementsByTagName("module");
+
+        // Itera a través de la lista de módulos.
+        for (int i = 0; i < modules.getLength(); i++) {
+
+            // Obtenemos el módulo actual
+            Element module = (Element) modules.item(i);
+
+            // Obtiene el nombre del modulo actual
+            String name = module.getElementsByTagName("name").item(0).getTextContent();
+
+            // Comprobamos si el módulo es igual al que buscamos.
+            if (name.equalsIgnoreCase(moduleName)) {
+                // Muestra su información.
+                System.out.println("Nombre: " + name);
+                System.out.println("Horas: " + module.getElementsByTagName("hours").item(0).getTextContent());
+                System.out.println("Tiempo: " + module.getElementsByTagName("time").item(0).getTextContent());
+                System.out.println("Estudiante: " + module.getElementsByTagName("student").item(0).getTextContent());
+                encontrado = true;
+                return;
             }
         }
+
+        // Imprimimos un mensaje si no lo hemos encontrado
+        if (!encontrado) {
+            System.out.println("El modulo " + moduleName + " no se ha encontrado");
+        }
+
     }
-    
+
+    /**
+     * Pregunta el nombre del módulo y te lo devuelve
+     *
+     * @return Un string que representa el nombre del módulo
+     */
     public static String preguntarBuscar() {
+
+        // Creamos un nuevo Scanner y preguntamos el módulo
         Scanner scanner = new Scanner(System.in);
         System.out.print("Ingrese el nombre del módulo a buscar: ");
         String moduleName = scanner.nextLine();
-        scanner.close(); // Cerrar el objeto Scanner.
+        // Cerramos el scanner
+        scanner.close();
         return moduleName;
-    }       
+    }
 }
 
 /**
  * Clase principal que ejecuta el programa
  */
-public class XMLPrograma {
+public class XMLProgram {
 
+    /**
+     *
+     * Ejecuta el programa, si tenemos la opcion write escribe en el fichero xml, si usamos la read lo lee y si ponemos search nos preguntara por un módulo en concreto y nos devolvera sus datos si existe. Si ponemos una opción inválida nos aparecera un mensaje indicandonoslo.
+     *
+     * @param args Los argumentos de línea de comandos que determinan la operación a realizar y los archivos XML involucrados.
+     * @throws IOException Si ocurre un error de entrada o salida al trabajar con archivos.
+     * @throws SAXException Si ocurre un error de análisis SAX al procesar un archivo XML.
+     * @throws ParserConfigurationException Si ocurre un error de configuración del analizador XML.
+     * @throws FileNotFoundException Si el archivo XML especificado no se encuentra.
+     * @throws TransformerException Si ocurre un error durante la transformación XML.
+     */
     public static void main(String[] args) throws IOException, SAXException, ParserConfigurationException, FileNotFoundException, TransformerException {
         fileXML dom = new fileXML();
-        
-        // Ejecutar si es read.
-        if (args[0].equalsIgnoreCase("read")) {
-            Document doc = dom.OpenXML(args[1]);
-            dom.printModules(doc.getDocumentElement());
-        }
-        // Ejecutar si es write
-        else if (args[0].equalsIgnoreCase("write")) {
-            File file = new File(args[1]);
-            if (!file.exists()) {
-                file.createNewFile();
+
+        try {
+            // Ejecutar si es read.
+            if (args[0].equalsIgnoreCase("read")) {
+                Document doc = dom.OpenXML(args[1]);
+                dom.printModules(doc.getDocumentElement());
+            } // Ejecutar si es write
+            else if (args[0].equalsIgnoreCase("write")) {
+                File file = new File(args[1]);
+                if (!file.exists()) {
+                    file.createNewFile();
+                }
+                dom.writeModules(file);
+            } // Ejecutar si es search
+            else if (args[0].equalsIgnoreCase("search")) {
+                String moduleName = fileXML.preguntarBuscar();
+                Document doc = dom.OpenXML(args[1]);
+                dom.findModule(moduleName, doc);
+            } // Ejecutar si no es valido
+            else {
+                System.out.println("Argumento inválido Posibles argumentos: read | write | search");
             }
-            dom.writeModules(file);
-        }
-        
-        else if (args[0].equalsIgnoreCase("search")){
-            String moduleName = fileXML.preguntarBuscar(); 
-            Document doc = dom.OpenXML(args[1]);
-            dom.findModule(moduleName, doc);
+        } catch (IOException | ParserConfigurationException | TransformerException | SAXException e) {
+            System.out.println("Numero de argumentos invalidos (opcion archivo.xml) Error: " + e);
         }
     }
 }
